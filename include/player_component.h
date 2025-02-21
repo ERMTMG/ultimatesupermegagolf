954fa89d457069f96501346844b0cd820c2dbc25
Eduardo <eduardorhsiete@gmail.com>
2025-02-21 20:12:20 +0100

    added basic functionality to PlayerComponent class + minor renamings

--- include/player_component.h
+++ include/player_component.h
@@ -4,6 +4,7 @@
 #include"vector2_utilities.h"
 #include"basic_components.h"
 #include"collision_component.h"
+#include"camera_view.h"
 
 /*
 Stores all the input information needed by the game: keys pressed, mouse info, etc
@@ -20,8 +21,9 @@ struct InputManager{
         RESET,          // 6
         INPUTS_ENUM_SIZE// 7
     };
-    std::bitset<INPUTS_ENUM_SIZE> keys;
-    std::bitset<INPUTS_ENUM_SIZE> keysLastFrame;
+    typedef std::bitset<INPUTS_ENUM_SIZE> InputMap;
+    InputMap keys = 0;
+    InputMap keysLastFrame = 0;
 };
 
 void update_input(InputManager& input);
@@ -29,14 +31,21 @@ bool is_input_active(const InputManager& input, InputManager::Inputs check);
 bool is_input_pressed_this_frame(const InputManager& input, InputManager::Inputs check);
 bool is_input_released_this_frame(const InputManager& input, InputManager::Inputs check);
 
+
+typedef unsigned char HPType;
 /*
 Stores variable information about the player: whether it can receive an impulse yet, 
 the current potential velocity it has, the number of times it's been impulsed, etc.
 */
 struct PlayerComponent{
-    Vector2 mouseAnchorPosition;
+    Position mouseAnchorPosition;
     Vector2 potentialVelocity;
-    unsigned int totalImpulses;
+    unsigned int totalImpulses = 0;
+    static const HPType MAX_HEALTH = 100;
+    HPType health = MAX_HEALTH;
     bool canDrag;
 };
+
+void update_player(PlayerComponent& player, Velocity& vel, const InputManager& input, const CameraView& camera);
+void release_player_drag_velocity(PlayerComponent& player, Velocity& vel);
 
--- src/player_component.cpp
+++ src/player_component.cpp
@@ -27,3 +27,34 @@ bool is_input_released_this_frame(const InputManager& input, InputManager::Input
     assert(check < InputManager::INPUTS_ENUM_SIZE);
     return (!input.keys[check] && input.keysLastFrame[check]);
 }
+
+void update_player(PlayerComponent& player, Velocity& vel, const InputManager& input, const CameraView& camera){
+    static const float MAX_IMPULSE_STRENGTH = 10 * M_2_PI; // don't take out the "*M_2_PI", it offsets the arctan's pi/2 limit
+    static const float IMPULSE_STRENGTH_SMOOTHNESS = 5; // controls the graduality of the curve from 0 impulse strength to max impulse strength
+    static const float VELOCITY_MARGIN_SQ = 0.01; // if the velocity's length is less than this value's square root then the player can drag again
+    if(player.canDrag){
+        if(is_input_pressed_this_frame(input, InputManager::MOUSE_CLICK)){
+            Vector2 positionInWorld = GetScreenToWorld2D(input.mouseScreenPosition, camera.cam);
+            player.mouseAnchorPosition = Position(positionInWorld);
+        } else if(is_input_active(input, InputManager::MOUSE_CLICK)){
+            Vector2 screenAnchor = GetWorldToScreen2D(to_Vector2(player.mouseAnchorPosition), camera.cam);
+            Vector2 difference = input.mouseScreenPosition - screenAnchor;
+            float totalStrength = MAX_IMPULSE_STRENGTH * atan(IMPULSE_STRENGTH_SMOOTHNESS * length(difference));
+            player.potentialVelocity = totalStrength * unit_vector(difference);
+        } else if(is_input_released_this_frame(input, InputManager::MOUSE_CLICK)){
+            release_player_drag_velocity(player, vel);
+        }
+
+        player.canDrag = (length_squared(to_Vector2(vel)) < VELOCITY_MARGIN_SQ);
+
+        //TODO later: do something with the health? still gotta program something that takes away health in the first place tho
+    }
+}
+
+void release_player_drag_velocity(PlayerComponent& player, Velocity& vel){
+    vel = Velocity(player.potentialVelocity);
+    player.potentialVelocity = {0,0};
+    player.mouseAnchorPosition = {0,0};
+    player.canDrag = false;
+    player.totalImpulses++;
+}
