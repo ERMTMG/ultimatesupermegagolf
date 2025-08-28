661f464a670a7e7437f1a31e11574ffea7e4c2ae
ERMTMG <eduardorhsiete@gmail.com>
2025-08-29 00:49:13 +0200

    added extra functionality to new main program (camera controls and ability to load other levels w/out recompiling)

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,30 @@
 
 const int SCREENWIDTH = 800;
 const int SCREENHEIGHT = 600;
-const char* const LEVEL_FILENAME = "resources/levels_json/test_level_colliders.json";
+static const char* LEVEL_FILENAME = "/home/eduardo-r/Projects/ultimatesupermegagolf/resources/levels_json/test_level_colliders.json";
 static bool DEBUG_MODE_ENABLED = false;
 
-int main(int argc, char** argv){
-    if(argc >= 2){
-        std::string arg{argv[1]};
-        if(arg.find("debug") != std::string::npos){
-            DEBUG_MODE_ENABLED = true;
+void parse_args(int argc, char** argv){
+    if(argc < 2){
+        return;
+    } else {
+        bool setLevelFilename = false;
+        for(int argIdx = 1; argIdx < argc; argIdx++){
+            const char* arg_i = argv[argIdx];
+            if(std::string(arg_i) == "-d" || std::string(arg_i) == "--debug"){
+                DEBUG_MODE_ENABLED = true;
+            } else if(!setLevelFilename){
+                LEVEL_FILENAME = arg_i;
+            } else {
+                std::cerr << "Too many arguments! Expected at most one level filename to load and an optional '-d' or '--debug' flag\n";
+                exit(-1);
+            }
         }
     }
+}
+
+int main(int argc, char** argv){
+    parse_args(argc, argv);
     InitWindow(SCREENWIDTH, SCREENHEIGHT, "Ultimate Super Mega Golf");
     SetTargetFPS(60);
     LevelRegistry level;
@@ -40,11 +54,12 @@ int main(int argc, char** argv){
         }
     }
 
+    CameraView& camera = *level.get_component<CameraView>(level.get_entity(level.CAMERA_ENTITY_NAME));
+
     while(!WindowShouldClose()){
         float delta = GetFrameTime();
         level.update(delta);
         level.draw(DEBUG_MODE_ENABLED);
-        /*
         if(IsKeyDown(KEY_KP_ADD)){
             zoom_camera(camera, 1.01, CAMERA_ZOOM_IN);
         } else if(IsKeyDown(KEY_KP_SUBTRACT)){
@@ -62,7 +77,12 @@ int main(int argc, char** argv){
         if(IsKeyDown(KEY_D)){
             move_camera(camera, {1, 0});
         }
-        */
+        if(IsKeyDown(KEY_Q)){
+            camera->rotation -= 1;
+        }
+        if(IsKeyDown(KEY_E)){
+            camera->rotation += 1;
+        }
         //std::cout << "camera coordinates: " << GetScreenToWorld2D({0,0}, camera.cam) << " to " << GetScreenToWorld2D({SCREENWIDTH, SCREENHEIGHT}, camera.cam) << '\n';
         //std::cout << "\tplayer position: " << to_Vector2(registry.get().get<Position>(registry.get_entity(registry.PLAYER_ENTITY_NAME))) << '\n';
     }
